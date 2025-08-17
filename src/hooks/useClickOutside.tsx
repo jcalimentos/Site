11256ea10129a78b1157238f4ff006b0ee2cf695
jcalimentos <docjcalimentos@gmail.com>
2025-08-17 16:04:16 -0300

    fix: error Hook

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -1,27 +1,25 @@
-import { useRef, useEffect } from 'react';
+import { useEffect, useRef, RefObject } from "react";
 
 export function useClickOutside<T extends HTMLElement>(
-  handler: (event: React.MouseEvent<T>) => void,
-  elementId?: string,
-): React.RefObject<T> {
-  const ref = useRef<T>(null);
-  useEffect(() => {
-    const handleClickOutside = (event: any) => {
-      if (!ref) return;
-      if (elementId) {
-        const element = document.getElementById(elementId);
-        if (element && element.contains(event.target)) return;
-      }
-      const { current } = ref;
-      if (current && !current.contains(event.target)) {
-        handler(event);
-      }
-    };
-    document.addEventListener('mouseup', handleClickOutside);
-    return () => {
-      document.removeEventListener('mouseup', handleClickOutside);
-    };
-  }, [ref, handler, elementId]);
+  handler: (event: MouseEvent) => void,
+  elementId?: string
+): RefObject<T> {
+  const ref = useRef<T | null>(null); // 👈 permite null
 
-  return ref;
+  useEffect(() => {
+    function listener(event: MouseEvent) {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    }
+
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [handler, elementId]);
+
+  // 👇 ajuste no retorno para casar com a tipagem esperada
+  return ref as RefObject<T>;
 }

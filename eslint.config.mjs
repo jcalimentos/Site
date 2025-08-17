1ec79c80f0c6b215ac2a1e14e43169518aa2ec8c
jcalimentos <docjcalimentos@gmail.com>
2025-08-17 15:56:20 -0300

    add:  Lint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next", "next/core-web-vitals"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-this-alias": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
